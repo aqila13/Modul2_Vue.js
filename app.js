9f835acb3698d8b13517749b02236c61224f8722
aqila13 <63911057+aqila13@users.noreply.github.com>
2021-01-27 22:36:40 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,35 +1,32 @@
 var app = new Vue({
     el: "#app",
-    data() {
-        return {
-            todos: [],
-            todo: ""
-        }
+    data : {
+        todos: [],
+        todo: ""
     },
-    created() {
+    created(){
         this.loadLocalStorage();
     },
-
-    watch: {
+    watch:{
         todos(){
-            localStorage.setItem("todos", JSON.stringify(this.todos));
+            localStorage.setItem("todos", JSON.stringify(this.todos))
         }
     },
-    methods: 
-    {
-        loadLocalStorage(){
-            const ls = JSON.parse(localStorage.getItem("todos"));
-            console.log(ls);
-            if(ls == null) {return; }
-            this.todos = ls;
-            console.log(this.todos);
-        },
-        addTodo (){
-            this.todos.push(this.todo);
-            this.todo = "";
-        },
-        deleteTodo(index){
-            this.todos.splice(index, 1);
-        },
+
+methods: {
+    loadLocalStorage(){
+        const ls = JSON.parse(localStorage.getItem("todos"));
+        console.log(ls);
+        if(ls == null) {return; }
+        this.todos = ls;
+        console.log(this.todos);
     },
+    addTodo : function(){
+        this.todos.push(this.todo);
+        this.todo = ""
+    },
+    deleteTodo: function(index){
+        this.todos.splice(index, 1);
+    },
+}
 })
